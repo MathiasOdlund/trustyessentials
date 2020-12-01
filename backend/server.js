96d2be3de484653761febc2c86297565c0559304
Mathias odlund <mathiasandre1998@gmail.com>
2020-12-01 13:48:22 +0100

    nit

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs')
-const morgan = require('morgan')
-const path = require('path')
+const fs = require("fs");
+const morgan = require("morgan");
+const path = require("path");
 //initializing some middleware morgan, db, etc.
 //Setting the amount of feedback needed from morgan, love user data;)
 // create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname + '/Trafic', 'access.log'), { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }))
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname + "/Trafic", "access.log"),
+  { flags: "a" }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
 
 //Simple test req, should remove later
-app.get('/', (req, res)=>{
-    res.send('hey')
-})
+app.get("/", (req, res) => {
+  res.send("hey");
+});
 
 //Listening for connections
 const port = process.env.PORT || 3001;
-app.listen(port, ()=>{
-    console.log(`Running server on prot ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Running server on prot ${port}`);
+});
